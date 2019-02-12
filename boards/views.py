01b7b4a4fba6c5a7ee2365bf018d74f658315c31
Xiaohui <hellolingxh@gmail.com>
2019-02-12 07:46:58 +0000

    add forms and test the forms then process request from client:
    using the plugin tool: django-widget-tweaks to set up the layout and style of html pages.

--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, Post, Topic
 from django.http import Http404
+
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -19,3 +22,24 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message = form.cleaned_data.get('message'),
+                topic = topic,
+                created_by = user
+            )
+            return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+    else:
+            form = NewTopicForm()
+        
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
