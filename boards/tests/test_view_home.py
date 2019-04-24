0401befd5237c73ac49e86e6b22e45c3f558aa49
xiaohui Ling <hellolingxh@gmail.com>
2019-04-24 19:12:21 +0100

    Develop (#1)
    * inital django web application
    
    * create a HELLO WORLD django web application
    
    using commands:
    1, pipenv install --skip-lock django
    2, django-admin startproject mydjangoproject
    3, django-admin startapp boards
    4, python3.6 manage.py runserver
    
    * create the objects such as Board, Topic, Post in boards/models.py file.
    
    using commands to view the pre-genernal sql statement:
    	python3.6 manage.py makemigrations  //it will produce the 0001_initial.py inside the boards/migrations directory.
    
    to view the sql statement:
    	python3.6 manage.py sqlmigrate boards 0001
    
    SQL instructions be executed in the database:
    	python3.6 manage.py migrate
    
    to enter into the interactive shell:
    	python3.6 manage.py shell
    	>> from boards.models import Board
    	>> board = Board(name='Django', description='This is a board about Django')
    	>> board.save()
    	>> board.id
    	>> board.name
    	>> board.description
    	>> board = Board.objects.create(name='Python', description='This is a board about Python')
    	>> board.id
    	>> board.name
    	>> Board.objects.all()
    
    to define the toString function for Models
    	def __str__(self):
    		return self.name
    
    to loop the list and print the item
    	board_list = Board.objects.all()
    	for board in board_list:
    		print(board.name)
    
    to query the board by criteria
    	django_board = Board.objects.get(id=1)
    
    * 1,add the template files
    	home.html
    
    2,add the static files
    	bootstrap.min.css
    
    3,add the test class define
    	test home view status code
    	test home url resolve home view
    
    4,run the commands:
    	python3.6 manage.py test
    	python3.6 manage.py test --verbosity=2 // 0 no outpu 1 normal output 2 verbosity output
    
    * Introduction to Django Admin
    
    create an administrator account:
    	python3.6 manage.py createsuperuser
    	username: admin
    	password: 000000
    
    visit url for back office:
    	http://localhost:8000/admin
    
    modify the admin.py to add the Board into back office models.
    	admin.site.register(Board)
    
    * 1,add URLs routing
    	/boards/{pk}/
    
    2,add testing for URLs routing using TDD
    
    * add forms and test the forms then process request from client:
    
    using the plugin tool: django-widget-tweaks to set up the layout and style of html pages.
    
    * fixed the bugs of the tests.py
    
    * implemented functions:
    1, user sign up
    2, user log in
    3, user change password
    4, user password reset
    
    * implemented functions:
    1, Protecting Views
    2, Authenticated User
    3, Topic Post View
    4, Reply Post View
    5, QuerySets
    6, add field to Migrations
    
    * The view strategies:
    1, function-based view
    2, class-based view
    3, generic class-base view
    
    implemented function:
    1,update view
    2,List view using paginator
    3,add my account view
    4,add markdown for editor
    5,add django.contrib.humanize for naturaltime
    6,add gravatar to refer the pic
    7,adjustment the paginator for short page number
    
    * add the post records for specific topic in db.sqlite3

---
+++ boards/tests/test_view_home.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+#from django.core.urlresolvers import reverse // Django 2.0 removes the django.core.urlresolvers module.
+from django.urls import reverse, resolve
+from ..views import home
+from ..models import Board
+from ..views import BoardListView
+
+class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func.view_class, BoardListView)#self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
